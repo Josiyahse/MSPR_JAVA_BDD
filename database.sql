4a0a03759252f98769eabe695bc663e196cef636
ErwanPro <erwan.monnier@aol.fr>
2021-05-11 17:11:02 +0200

    feat(table): update BDD

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Drone(
    id_drone INT,
-   libelle VARCHAR(150),
+   libelle VARCHAR(50),
    PRIMARY KEY(id_drone)
 );
 
 CREATE TABLE Interieur(
    id_interieur INT,
-   libelle VARCHAR(150),
+   libelle VARCHAR(50),
    PRIMARY KEY(id_interieur)
 );
 
@@ -19,10 +19,10 @@ CREATE TABLE Corps_metier(
 CREATE TABLE Sous_traitant(
    id_sous_traitant INT,
    nom VARCHAR(50),
-   rue VARCHAR(200),
-   code_postal INT(5),
+   rue VARCHAR(50),
+   code_postale VARCHAR(50),
    ville VARCHAR(50),
-   id_corps_metier INT,
+   id_corps_metier INT NOT NULL,
    PRIMARY KEY(id_sous_traitant),
    FOREIGN KEY(id_corps_metier) REFERENCES Corps_metier(id_corps_metier)
 );
@@ -31,21 +31,23 @@ CREATE TABLE Personne(
    id_personne INT,
    nom VARCHAR(50),
    prenom VARCHAR(50),
-   mail VARCHAR(100),
+   mail VARCHAR(50),
    PRIMARY KEY(id_personne)
 );
 
 CREATE TABLE Projet(
    id_projet INT,
    nom VARCHAR(50),
-   debut DATE,
+   date_debut DATE,
+   date_fin DATE,
    PRIMARY KEY(id_projet)
 );
 
 CREATE TABLE Chantier(
    id_chantier INT,
    nom VARCHAR(50),
-   debut DATE,
+   date_debut DATE,
+   date_fin DATE,
    PRIMARY KEY(id_chantier)
 );
 
@@ -57,11 +59,11 @@ CREATE TABLE Role(
 );
 
 CREATE TABLE Profil(
-   username VARCHAR(50),
+   username INT,
    nom VARCHAR(50),
    mdp VARCHAR(50),
-   date_modification DATE,
-   est_interne NUMBER(1),
+   date_modification VARCHAR(50),
+   est_interne VARCHAR(50),
    id_personne INT NOT NULL,
    PRIMARY KEY(username),
    UNIQUE(id_personne),
@@ -71,13 +73,13 @@ CREATE TABLE Profil(
 CREATE TABLE Capture(
    id_capture INT,
    date_capture DATE,
-   heure_capture VARCHAR(10),
-   coordonnee_x DECIMAL,
-   coordonnee_y DECIMAL,
+   heure_capture VARCHAR(50),
+   coordonnee_x VARCHAR(50),
+   coordonnee_y VARCHAR(50),
    jalon_gant VARCHAR(50),
    etape_capture VARCHAR(50),
-   incident VARCHAR(200),
-   lien VARCHAR(100),
+   incident VARCHAR(50),
+   lien VARCHAR(50),
    id_chantier INT NOT NULL,
    id_projet INT NOT NULL,
    id_interieur INT,
@@ -87,14 +89,6 @@ CREATE TABLE Capture(
    FOREIGN KEY(id_projet) REFERENCES Projet(id_projet),
    FOREIGN KEY(id_interieur) REFERENCES Interieur(id_interieur),
    FOREIGN KEY(id_drone) REFERENCES Drone(id_drone)
-);
-
-CREATE TABLE Cibler(
-   id_capture INT,
-   id_corps_metier INT,
-   PRIMARY KEY(id_capture, id_corps_metier),
-   FOREIGN KEY(id_capture) REFERENCES Capture(id_capture),
-   FOREIGN KEY(id_corps_metier) REFERENCES Corps_metier(id_corps_metier)
 );
 
 CREATE TABLE Concerner(
@@ -113,7 +107,7 @@ CREATE TABLE Posseder(
    FOREIGN KEY(id_personne) REFERENCES Personne(id_personne)
 );
 
-CREATE TABLE Beneficier(
+CREATE TABLE Bénéficier(
    id_personne INT,
    id_role INT,
    PRIMARY KEY(id_personne, id_role),
@@ -121,3 +115,10 @@ CREATE TABLE Beneficier(
    FOREIGN KEY(id_role) REFERENCES Role(id_role)
 );
 
+CREATE TABLE Cibler(
+   id_corps_metier INT,
+   id_chantier INT,
+   PRIMARY KEY(id_corps_metier, id_chantier),
+   FOREIGN KEY(id_corps_metier) REFERENCES Corps_metier(id_corps_metier),
+   FOREIGN KEY(id_chantier) REFERENCES Chantier(id_chantier)
+);
